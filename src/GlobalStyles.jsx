8cef3065f69acc29dd0e7763dc3184641120ef35
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-01-05 22:18:29 +0900

    Feature: 로그인 페이지 디자인 (#14)
    * style: 브라우저 기본 margin, padding 제거거
    
    * docs: 로고 파일 변경
    
    * feat: 로그인 Form 구현
    
    * feat: 로그인페이지 구현
    
    * feat: 로고 클릭 시 메인으로 이동 기능 추가

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -4,6 +4,8 @@ const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
     scrollbar-width: thin;
+    margin: 0;
+    padding: 0;
   }
   body {
     font-family : pretendard;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import SignInForm from "../components/sign-in/SignInForm";
+
 const SignIn = () => {
-  return <div>로그인 페이지 입니다.</div>;
+  return (
+    <Container>
+      <ContentContainer>
+        <SignInForm />
+      </ContentContainer>
+    </Container>
+  );
 };
 
 export default SignIn;
+
+const Container = styled.div`
+  display: flex;
+  background: linear-gradient(to bottom, #bbbbbb, white);
+  height: 50vh;
+  width: 100%;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 56px);
+  height: 100vh;
+`;
