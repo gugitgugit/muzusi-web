5b22765a3919a1212c129fc07f4fdfff06e7b41c
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-01-19 16:32:26 +0900

    Fix: 회원가입 시 저장된 닉네임 변경 (#27)
    * fix: 회원가입 시 닉네임 정보가 갱신되지 않는 현상 수정
    
    * feat: 예외성 검사 로직 추가

--- src/components/sign-up/SignUpForm.jsx
+++ src/components/sign-up/SignUpForm.jsx
@@ -3,9 +3,11 @@ import MuzusiLogo from "@/assets/logo/MuzusiLogo.png";
 import { useState } from "react";
 import postNickname from "../../api/sign-up/postNickname";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/useAuth";
 
 const SignUpForm = () => {
   const navigate = useNavigate();
+  const { login } = useAuth();
 
   const [nickname, setNickname] = useState("");
   const [available, setAvailable] = useState(
@@ -37,6 +39,7 @@ const SignUpForm = () => {
         const response = await postNickname(nickname);
         if (response.code === 200) {
           console.log("닉네임 등록 성공", response);
+          login({ token: response.data.accessToken });
           alert("닉네임 등록이 완료되었습니다.");
           navigate("/");
         } else {
@@ -44,6 +47,9 @@ const SignUpForm = () => {
           setError("닉네임 등록 중 문제가 발생했습니다. 다시 시도해주세요.");
         }
       } catch (apiError) {
+        if (apiError.reponse.status === 422) {
+          alert("닉네임 형식을 확인해주세요.");
+        }
         console.error("닉네임 등록 실패", apiError);
         setError("닉네임 등록 중 문제가 발생했습니다. 다시 시도해주세요.");
       }
